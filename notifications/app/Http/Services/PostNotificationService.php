91ddfaf53c2239788d8c168c8285d15f65a0a098
Hayk <ayk.khachatryan.1994@gmail.com>
2024-03-18 01:28:01 +0400

    All logic done

--- notifications/app/Http/Services/PostNotificationService.php
+++ notifications/app/Http/Services/PostNotificationService.php
@@ -12,7 +12,22 @@ class PostNotificationService
     {
 
         $data = json_Decode($message);
-        // PostNotification::create($data);
+
+        $mainAction = match ($data->action) {
+            "post_created" => "created",
+            "post_updated" => "updated",
+            "post_deleted" => "deleted",
+            default => "changed.",
+        };
+
+        $actionMessage = "Blog $data->author was $mainAction by $data->author in $data->publication_year year";
+
+
+        $newNotification = [
+            "post_id" => $data->post_id,
+            "message" => $actionMessage,
+        ];
+        PostNotification::create($newNotification);
 
         return true;
     }
